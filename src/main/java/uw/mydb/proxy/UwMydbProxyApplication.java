e6f3dc6591915096b70483ecdd5f8d10575f317e
zhangjin <23231269@qq.com>
2025-09-15 21:58:51 +0800

    refactor(uw-mydb-proxy): 修改 Spring Boot 应用类型- 从 SpringApplication.run() 更改为 SpringApplicationBuilder - 设置 WebApplicationType 为 NONE，表明不使用 Web 功能 - 保持 MySqlClient 和 ProxyServer 的启动逻辑不变

--- src/main/java/uw/mydb/proxy/UwMydbProxyApplication.java
+++ src/main/java/uw/mydb/proxy/UwMydbProxyApplication.java
@@ -1,7 +1,8 @@
 package uw.mydb.proxy;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import uw.mydb.proxy.mysql.MySqlClient;
 import uw.mydb.proxy.server.ProxyServer;
@@ -11,7 +12,9 @@ import uw.mydb.proxy.server.ProxyServer;
 public class UwMydbProxyApplication {
 
     public static void main(String[] args) throws InterruptedException {
-        SpringApplication.run( UwMydbProxyApplication.class, args );
+        new SpringApplicationBuilder(UwMydbProxyApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
         //启动mysql客户端
         MySqlClient.start();
         //代理服务器启动
